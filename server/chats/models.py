99d98f24505d982308a85e755c20ad77ab36aa62
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2026-01-31 13:25:59 -0600

    Added Chat model and updated gitignore and README

---
+++ server/chats/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Chat(models.Model):
+    guest = models.CharField(max_length=50)
+    chat_date = models.DateTimeField("date chatted")
+    notes = models.TextField()
